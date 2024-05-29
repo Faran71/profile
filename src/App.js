4237a08a7d52cd9d3611422d433d4b4f719f28cb
Muhammad Faran Sarwar <muhammadfaran01.mfs@gmail.com>
2024-05-29 13:55:17 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import MainPage from './containers/MainPage';
+import { Route, Router, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route>
+          <Route path='/' 
+          element={<MainPage />} 
+          key={1}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <MainPage />
     </div>
   );
